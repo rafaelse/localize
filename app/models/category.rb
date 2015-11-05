f96b0a3d1a4eacd3d084ae70814ee11fa252f872
Rafael santos <rfs.rafael@gmail.com>
2015-11-05 03:54:27 -0500

    Typo in Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :subcategories, :class_name => "Category", :foreign_key => "parent_id", :dependent => :destroy
   belongs_to :parent_category, :class_name => "Category", foreign_key: "parent_id"
-  has_many :promotions, dependent: :null
+  has_many :promotions, dependent: :nullify
   
   def depth
     unless @depth
